ce9e96cd79969c4055ac8ba2b50e199346057e28
Corey <corey@coreys-macbook-pro.local>
2016-06-29 10:49:26 -0700

    Initial Commit

---
+++ iMagine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  iMagine
+//
+//  Created by Corey on 2016-06-29.
+//  Copyright © 2016 Corey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
